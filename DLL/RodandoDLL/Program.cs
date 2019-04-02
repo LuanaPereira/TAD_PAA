c9cd903383785942c32be7a5c680c82fb88688b7
Luana Pereira <37309259+LuanaPereira@users.noreply.github.com>
2019-04-02 13:43:00 -0300

    Add files via upload

---
+++ DLL/RodandoDLL/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using TesteDLL;
+namespace RodandoDLL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(TesteDLL.Class1.soma(12, 8));
+            
+        }
+    }
+}
---
+++ DLL/TesteDLL/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TesteDLL
+{
+    public class Class1
+    {
+        public static int soma(int a, int b)
+        {
+            return (a + b);
+        }
+    }
+}
